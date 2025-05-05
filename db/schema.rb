2a1088bd64f2181181aa3d4c5323dea0e1a46abf
Ryuki Isami <isamin1211.program@gmail.com>
2025-05-05 17:29:29 +0900

    20250505_add:Towns table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_05_05_073037) do
+ActiveRecord::Schema[7.2].define(version: 2025_05_05_080947) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,12 @@ ActiveRecord::Schema[7.2].define(version: 2025_05_05_073037) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_spots_on_user_id"
+  end
+
+  create_table "towns", force: :cascade do |t|
+    t.string "address", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
