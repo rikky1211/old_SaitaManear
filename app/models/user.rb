9c4f438adbd3ff08048b6a6efb4756af002eac86
Ryuki Isami <isamin1211.program@gmail.com>
2025-05-07 12:19:11 +0900

    20250507_add:User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
+
+  enum role: { general: 0, admin: 1 }
+  
 end
