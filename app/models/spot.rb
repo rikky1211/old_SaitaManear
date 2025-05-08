74e6ded8f9f74c682ad0a01a2b655c5387e177d2
Ryuki Isami <isamin1211.program@gmail.com>
2025-05-08 14:57:55 +0900

    20250508_fix:model_spotの緯度経度にuniquenessを追加。同じ値の時に地図が表示されなくなる

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,7 +1,7 @@
 class Spot < ApplicationRecord
   validates :spot_image, presence: true
-  validates :latitude, presence: true
-  validates :longitude, presence: true
+  validates :latitude, presence: true, uniqueness: true
+  validates :longitude, presence: true, uniqueness: true
 
   belongs_to :user
   belongs_to :town
